90c6dbb3d64c653208189ce707fbc11e25dcd67a
NelanJoe <nelanjorelat@gmail.com>
2024-05-12 19:22:46 +0700

    add item orderby

--- src/service/items.service.js
+++ src/service/items.service.js
@@ -1,10 +1,17 @@
-import { collection, getDocs, addDoc } from "firebase/firestore";
+import {
+  collection,
+  getDocs,
+  addDoc,
+  query,
+  orderBy,
+} from "firebase/firestore";
 import db from "../lib/firebase/db";
 
 export const getAllItems = async () => {
   try {
+    const itemsRef = collection(db, "items");
     const querySnapshot = await getDocs(
-      collection(db, "items").orderBy("", "asc")
+      query(itemsRef, orderBy("name", "asc"))
     );
     return querySnapshot.docs.map((doc) => ({ ...doc.data(), id: doc.id }));
   } catch (error) {
